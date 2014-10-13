6749a49e6d2a82d16a39afab783f06e611e0c329
serzilo <designkota@mail.ru>
2014-10-13 16:33:46 +0400

    added gestures, fixes

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,7 @@
-$('#toggle_menu').on('click',function(e){
-	e.preventDefault();
-	e.stopPropagation();
-	Sidebar.toggle_menu(1);
-});
 
-$('#shadow').on('click',function(e){
-	e.preventDefault();
-	e.stopPropagation();
-	Sidebar.toggle_menu(0);
-});
-
-$('#fixed_button').on('click',function(e){
-	e.preventDefault();
-	e.stopPropagation();
-	Sidebar.toggle_menu();
-});
 
 var Sidebar = {
+	top_scroll_state : 0,
 	toggle_menu : function(a){
 		var page = $('.page'),
 			content = $('#content'),
@@ -64,5 +49,96 @@ var Sidebar = {
 		}
 	},
 
-	top_scroll_state : 0
+	swipe: function(){
+	    var main = document.getElementById('main__inner'),
+			inGesture = false,
+			startX = 0, startY = 0,
+			yMaxDistance = 60,
+			xGestureDistance = 50;
+	    
+	    function mousedown(event){
+			inGesture = true;
+			startX = (event.touches) ? event.touches[0].pageX : event.pageX;
+			startY = (event.touches) ? event.touches[0].pageY : event.pageY;
+
+			if (event.touches && event.touches.length!=1) {
+			   inGesture = false;
+			}
+	    }
+	    
+	    function mousemove(event){
+			if (inGesture){
+			    var deltaX = 0, deltaY = 0,
+				direction;
+			    
+			    var moveX = (event.touches) ? event.touches[0].pageX : event.pageX,
+				moveY = (event.touches) ? event.touches[0].pageY : event.pageY;
+
+			    deltaX = Math.abs(moveX - startX);
+			    deltaY = Math.abs(moveY - startY);
+			    
+			    if (deltaY > yMaxDistance){
+					inGesture = false;
+			    }
+			    
+			    if (inGesture){
+					if ((deltaX * 0.66) >= deltaY){
+					    if(navigator.userAgent.match(/Android/i)){
+							event.preventDefault();
+					    }
+
+					    if (startX > moveX){
+							direction = "left";
+					    }else{
+							direction = "right";
+					    }
+					    
+					    if (deltaX >= xGestureDistance){
+					    	var page = $('.page');
+
+							if (direction == "left"){
+								if (page.hasClass('page_menu-opened')){
+							    	Sidebar.toggle_menu(0);
+								}
+							}else if (direction == "right"){
+								if (!page.hasClass('page_menu-opened')){
+							    	Sidebar.toggle_menu(1);
+								}
+							}
+							inGesture = false;
+					    }
+					}else{
+					    inGesture = false;
+					}
+				}
+			}
+	    }
+
+	    main.addEventListener('touchstart', mousedown, false);
+	    main.addEventListener('touchmove', mousemove, false);
+	},
+
+	init : function(){
+		Sidebar.swipe();
+
+		$('#toggle_menu').on('click',function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			Sidebar.toggle_menu(1);
+		});
+
+		$('#shadow').on('click',function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			Sidebar.toggle_menu(0);
+		});
+
+		$('#fixed_button').on('click',function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			Sidebar.toggle_menu();
+		});
+	}
 };
+
+Sidebar.init();
